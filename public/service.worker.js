760298cdde33d2a24425beebe2c6417d2370eec4
sennin361 <pokeopoke9@gmail.com>
2025-05-30 07:06:39 +0900

    Create service.worker.js

---
+++ public/service.worker.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = "sennin-chat-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/chat.html",
+  "/admin.html",
+  "/main.js",
+  "/style.css",
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png",
+  "/favicon.ico"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return (
+        response ||
+        fetch(event.request).catch(() =>
+          caches.match("/offline.html")
+        )
+      );
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+});
